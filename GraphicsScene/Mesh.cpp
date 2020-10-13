8b6561c365344960cc541405fae71c128b71476b
Van Phelan <van.phelan@aie.edu>
2020-10-13 16:21:13 -0500

    Created generic initialize function for Mesh

--- GraphicsScene/Mesh.cpp
+++ GraphicsScene/Mesh.cpp
@@ -8,7 +8,9 @@ Mesh::~Mesh()
 	glDeleteBuffers(1, &m_ibo);
 }
 
-void Mesh::initializeQuad()
+void Mesh::initialize(
+	unsigned int vertexCount, const Vertex* vertices,
+	unsigned int indexCount, unsigned int* indices)
 {
 	//Check that the mesh is not already initialized
 	assert(m_vao == 0);
@@ -21,29 +23,10 @@ void Mesh::initializeQuad()
 	glBindVertexArray(m_vao);
 	glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
 
-	//Define 6 vertices for 2 triangles
-	Vertex vertices[6];
-	//Triangle A
-	vertices[0].position = { -0.5f, 0.0f, 0.5f, 1 };
-	vertices[1].position = { 0.5f, 0.0f, 0.5f, 1 };
-	vertices[2].position = { -0.5f, 0.0f, -0.5f, 1 };
-	//Triangle B
-	vertices[3].position = { -0.5f, 0.0f, -0.5f, 1 };
-	vertices[4].position = { 0.5f, 0.0f, 0.5f, 1 };
-	vertices[5].position = { 0.5f, 0.0f, -0.5f, 1 };
-	//Triangle A
-	vertices[0].color = { 0.2f, 0.2f, 0.8f, 1 };//blue
-	vertices[1].color = { 0.8f, 0.8f, 0.2f, 1 };//yellow
-	vertices[2].color = { 0.2f, 0.8f, 0.8f, 1 };//cyan
-	//Triangle B
-	vertices[3].color = { 0.2f, 0.8f, 0.2f, 1 };//green
-	vertices[4].color = { 0.8f, 0.2f, 0.8f, 1 };//magenta
-	vertices[5].color = { 0.8f, 0.2f, 0.2f, 1 };//red
-
 	//Fill vertex buffer
 	glBufferData(
 		GL_ARRAY_BUFFER,
-		6 * sizeof(Vertex),
+		vertexCount * sizeof(Vertex),
 		vertices,
 		GL_STATIC_DRAW
 	);
@@ -69,12 +52,56 @@ void Mesh::initializeQuad()
 		(GLvoid*)(8 * sizeof(float))
 	);
 
+	//Bind indices if there are any
+	if (indexCount != 0) {
+		//Generate index buffer
+		glGenBuffers(1, &m_ibo);
+
+		//Bind index buffer
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ibo);
+
+		//Fill index buffer
+		glBufferData(
+			GL_ELEMENT_ARRAY_BUFFER,
+			indexCount * sizeof(unsigned int),
+			indices,
+			GL_STATIC_DRAW
+		);
+
+		m_triCount = indexCount / 3;
+	}
+	else {
+		m_triCount = vertexCount / 3;
+	}
+
 	//Unbind buffers
 	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
 
-	//Quad has two trianges
-	m_triCount = 2;
+void Mesh::initializeQuad()
+{
+	//Define 6 vertices for 2 triangles
+	Vertex vertices[6];
+	//Triangle A
+	vertices[0].position = { -0.5f, 0.0f, 0.5f, 1 };
+	vertices[1].position = { 0.5f, 0.0f, 0.5f, 1 };
+	vertices[2].position = { -0.5f, 0.0f, -0.5f, 1 };
+	//Triangle B
+	vertices[3].position = { -0.5f, 0.0f, -0.5f, 1 };
+	vertices[4].position = { 0.5f, 0.0f, 0.5f, 1 };
+	vertices[5].position = { 0.5f, 0.0f, -0.5f, 1 };
+	//Triangle A
+	vertices[0].color = { 0.2f, 0.2f, 0.8f, 1 };//blue
+	vertices[1].color = { 0.8f, 0.8f, 0.2f, 1 };//yellow
+	vertices[2].color = { 0.2f, 0.8f, 0.8f, 1 };//cyan
+	//Triangle B
+	vertices[3].color = { 0.2f, 0.8f, 0.2f, 1 };//green
+	vertices[4].color = { 0.8f, 0.2f, 0.8f, 1 };//magenta
+	vertices[5].color = { 0.8f, 0.2f, 0.2f, 1 };//red
+
+	initialize(6, vertices);
 }
 
 void Mesh::draw()
--- GraphicsScene/Mesh.h
+++ GraphicsScene/Mesh.h
@@ -4,20 +4,25 @@
 class Mesh
 {
 public:
-	Mesh() : m_triCount(0), m_vao(0), m_vbo(0), m_ibo(0) {}
-	~Mesh();
-
-	void initializeQuad();
-
-	virtual void draw();
-
-public:
 	struct Vertex {
 		glm::vec4 position;
 		glm::vec4 normal;
 		glm::vec4 color;
 		glm::vec2 texCoord;
 	};
+
+public:
+	Mesh() : m_triCount(0), m_vao(0), m_vbo(0), m_ibo(0) {}
+	~Mesh();
+
+	void initialize(
+		unsigned int vertexCount,
+		const Vertex* vertices,
+		unsigned int indexCount = 0,
+		unsigned int* indices = nullptr);
+	void initializeQuad();
+
+	virtual void draw();
 
 protected:
 	unsigned int m_triCount;
